77d712e77766ed0b43aebdb3530d3fa011082acb
Jacob Terrell <jacobit97@hotmail.com>
2021-04-27 14:27:06 -0600

    small updates

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,17 @@
 import puppeteer, { Browser, Page } from 'puppeteer';
 import chromium from 'chrome-aws-lambda';
-import aws, { DynamoDB } from 'aws-sdk';
-
-
-
+import aws from 'aws-sdk';
 
 const documentClient = new aws.DynamoDB.DocumentClient();
 const s3 = new aws.S3();
 const bucket = "screenshot-audible";
 const key = "audible.png";
-
 const { v4: uuidv4 } = require('uuid');
-const { addExtra } = require('puppeteer-extra');
-const pluginStealth = require('puppeteer-extra-plugin-stealth');
 
-function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    });
-}
+exports.handler = async () => {
 
-exports.handler = async (event) => {
-    const args = [
-        // '--proxy-server=zproxy.lum-superproxy.io:22225'
-    ];
-
-    args.push(...chromium.args);
-    const browser = await chromium.puppeteer.launch({
-        args: args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true,
-    });
     try {
-        const bookDetails = await getBookDetails(browser);
-        await browser.close();
-
+        const bookDetails = await getBookDetails();
         return bookDetails;
 
     } catch (e) {
@@ -44,130 +19,128 @@ exports.handler = async (event) => {
         const params = { Bucket: bucket, Key: key, Body: screenshot };
         await s3.putObject(params).promise();
         console.log('something happened See screenshot in s3 bucket', e.error);
-        await browser.close();
+
         return e.error;
     }
+
 }
 
-async function getBookDetails(browser) {
+async function getBookDetails() {
+    const bookData = [];
+    const cookies = [
+        process.env.jordanCookie,
+        // process.env.jonCookie,
+        process.env.ashliCookie
+    ];
     const url = 'https://www.audible.com/';
-    const context = await browser.createIncognitoBrowserContext();
-    const page = await auth(context, process.env.jordanCookie);
+    const args = [];
+    args.push(...chromium.args);
     try {
-        // await page.goto(url);
-        // await page.click('.ui-it-sign-in-link');
-        // await page.waitForSelector('#ap_email', { timeout: 5000 });
-        // await page.type('#ap_email', process.env.email);
-        // await page.type('#ap_password', process.env.password);
+        for (let i = 0; i < cookies.length; i++) {
 
-        // await page.click('#signInSubmit');
+            const browser = await chromium.puppeteer.launch({
+                args: args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless,
+                ignoreHTTPSErrors: true,
+            });
 
-        // console.log("Sign in link");
-        // console.log("before titles page");
-        // await page.waitForSelector('.bc-pub-block.ui-it-header-logo', { timeout: 5000 });
-
-        console.log("after titles page");
-        const pageNumberLast: number = +await page.$eval('.linkListWrapper li:nth-last-of-type(2) .bc-link', element => element.getAttribute('data-value'));
-        console.log('page number', pageNumberLast);
-        // const pageNumber: number = +pageNumberString;
-        let pageNumber: number = 1;
-        // let libraryHasBooks = true;
-        const bookData: any[] = [];
-        while (pageNumber <= pageNumberLast) {
-            await page.goto(`${url}/library/titles?ref=a_library_t_c6_pageSize_0&pf_rd_p=754864b9-4c5c-4301-b92a-69b12a5623c4&pf_rd_r=2793CX9HNJGABE75EYEX&piltersCurrentValue=All&sortBy=PURCHASE_DATE.dsc&pageSize=20&page=${pageNumber}`);
-
-            const booksHandle = await page.$$('.adbl-library-content-row');
-            for (let bookHandle of booksHandle) {
-                const title = await bookHandle.$eval('.bc-text.bc-size-headline3', element => element.textContent);
-                const author = await bookHandle.$eval('.bc-text.bc-size-callout', element => element.textContent);
-                const image = await bookHandle.$eval('img', element => element.getAttribute('src'));
-                let url: string;
-                try {
-                    url = await bookHandle.$eval('.bc-list-item:nth-of-type(1) .bc-link.bc-color-base', element => element.getAttribute('href'));
-                }
-                catch (e) {
-                    console.log('Cannot find url. Moving on.');
-                }
-                bookData.push({
-                    title: title.trim(),
-                    author: author.trim(),
-                    image: image,
-                    url: url,
-                });
+            const page = await auth(browser, cookies[i]);
+            let pageNumberLast: number;
+            try {
+                pageNumberLast = +await page.$eval('.linkListWrapper li:nth-last-of-type(2) .bc-link', element => element.getAttribute('data-value'));
+            } catch (e) {
+                pageNumberLast = 2;
             }
-            pageNumber++;
 
+            console.log('page number', pageNumberLast);
+            let owner = await page.$eval('.bc-text.navigation-do-underline-on-hover.ui-it-barker-text', element => element.textContent);
+            owner = owner.split(',')[1].replace('!', '').trim();
+            let pageNumber: number = 1;
+            const existingTitles = await scanTitles();
+
+            while (pageNumber <= pageNumberLast) {
+                await page.goto(`${url}/library/titles?ref=a_library_t_c6_pageSize_0&pf_rd_p=754864b9-4c5c-4301-b92a-69b12a5623c4&pf_rd_r=2793CX9HNJGABE75EYEX&piltersCurrentValue=All&sortBy=PURCHASE_DATE.dsc&pageSize=20&page=${pageNumber}`);
+
+                const booksHandle = await page.$$('.adbl-library-content-row');
+                for (let bookHandle of booksHandle) {
+                    const title = await bookHandle.$eval('.bc-text.bc-size-headline3', element => element.textContent);
+                    const author = await bookHandle.$eval('.bc-text.bc-size-callout', element => element.textContent);
+                    const picture = await bookHandle.$eval('.bc-image-inset-border', element => element.getAttribute('src'));
+                    let url: string;
+                    try {
+                        url = await bookHandle.$eval('.bc-list-item:nth-of-type(1) .bc-link.bc-color-base', element => element.getAttribute('href'));
+                    }
+                    catch (e) {
+                        console.log('Cannot find url. Moving on.');
+                    }
+                    if (!existingTitles.find(book => book.title === title) && url) {
+                        bookData.push({
+                            title: title.trim(),
+                            author: author.trim(),
+                            url: url,
+                            owner: owner,
+                            image: picture
+                        });
+                    }
+
+
+                }
+                pageNumber++;
+
+            }
+
+            console.log('bookData', bookData);
+            for (let i = 0; i < bookData.length; i++) {
+
+                const params = {
+                    TableName: 'audible-libraries',
+                    Item: {
+                        id: uuidv4(),
+                        title: bookData[i].title,
+                        author: bookData[i].author,
+                        url: bookData[i].url,
+                        owner: bookData[i].owner,
+                        image: bookData[i].image
+                    }
+                };
+                await documentClient.put(params).promise();
+            }
+
+            await browser.close();
         }
 
-        console.log('bookData', bookData);
-        // const inputParams: aws.DynamoDB.DocumentClient.PutItemInput = {
-        //     Item: bookData,
-        //     TableName: 'audible-libraries'
-        // };
-        const params = {
-            TableName: 'audible-libraries',
-            Item: {
-                id: uuidv4(),
-                title: bookData[0].title,
-                author: bookData[0].author,
-
-            }
-        };
-        await documentClient.put(params).promise();
-        await context.close();
-
-        return bookData;
-    } catch (e) {
-        const screenshot = await page.screenshot();
-        const params = { Bucket: bucket, Key: key, Body: screenshot };
-        await s3.putObject(params).promise();
-        console.log('something happened See screenshot in s3 bucket', e);
-        await context.close();
     }
-
-
+    catch (e) {
+        console.log('something happened See screenshot in s3 bucket', e);
+    }
+    return bookData;
 }
 
-// async function auth(context: any, cookie: string) {
+async function scanTitles(existingTitles = [], lastEvaluatedKey?: aws.DynamoDB.DocumentClient.Key) {
 
-//     const page = await context.newPage();
+    const scanParams: aws.DynamoDB.DocumentClient.ScanInput = {
+        TableName: 'audible-libraries',
+        ProjectionExpression: 'title'
+    };
+    if (lastEvaluatedKey) {
+        scanParams.ExclusiveStartKey = lastEvaluatedKey;
+    }
+    const titleResponse = await documentClient.scan(scanParams).promise();
+    existingTitles.push(...titleResponse.Items);
 
-//     const cookies: puppeteer.Cookie[] = [
-//         {
-//             name: "x-main",
-//             value: cookie,
-//             domain: ".audible.com",
-//             path: '/'
+    if (titleResponse.LastEvaluatedKey) {
+        return await scanTitles(existingTitles, titleResponse.LastEvaluatedKey);
+    }
 
-//         } as puppeteer.Cookie
-//     ];
+    return existingTitles;
+}
 
-//     await page.goto('https://audible.com');
-//     // Set cookie and then go to library
-//     await page.setCookie(...cookies);
-//     await page.goto('https://audible.com/library/titles');
-//     await page.waitForSelector('.post_view').catch((err) => console.log('Unable to get a post_view element'));
-//     const screenshot = await page.screenshot();
-//     const params = { Bucket: 'audible', Key: 'a1udible.png', Body: screenshot };
-//     await s3.putObject(params).promise();
-//     try {
-//         await page.waitForSelector('.adbl-library-content-row', { timeout: 9000 });
-//     } catch (e) {
-//         await page.goto('https://audible.com/library/titles');
-//         return page;
-//     }
-
-//     return page;
-// }
 
 async function auth(browser: any, cookie: string) {
 
     const page = await browser.newPage();
-    // await page.authenticate({
-    //     username: process.env.luminatiUsername,
-    //     password: process.env.luminatiPassword
-    // });
-
     const cookies: puppeteer.Cookie[] = [
         {
             name: "x-main",
--- src/indexHolder.ts
+++ src/indexHolder.ts
@@ -1,79 +1,68 @@
 import puppeteer, { Browser, Page } from 'puppeteer';
 import chromium from 'chrome-aws-lambda';
-import aws, { DynamoDB } from 'aws-sdk';
-
-
-
+import aws from 'aws-sdk';
 
 const documentClient = new aws.DynamoDB.DocumentClient();
 const s3 = new aws.S3();
 const bucket = "screenshot-audible";
 const key = "audible.png";
-
 const { v4: uuidv4 } = require('uuid');
-const { addExtra } = require('puppeteer-extra');
-const pluginStealth = require('puppeteer-extra-plugin-stealth');
 
-function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    });
-}
+exports.handler = async () => {
+    const args = [];
+    args.push(...chromium.args);
+    const cookies = [
+        process.env.jordanCookie,
+        // process.env.jonCookie,
+        process.env.ashliCookie
+    ];
+    for (let i = 0; i < cookies.length; i++) {
 
-exports.handler = async (event) => {
-    const puppeteerExtra = addExtra(chromium.puppeteer);
-    puppeteerExtra.use(pluginStealth());
-
-
-    const browser: Browser = await puppeteerExtra
-        .launch({
-            args: chromium.args,
+        const browser = await chromium.puppeteer.launch({
+            args: args,
             defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath,
             headless: chromium.headless,
+            ignoreHTTPSErrors: true,
         });
-    try {
-        const bookDetails = await getBookDetails(browser);
-        await browser.close();
 
-        return bookDetails;
+        try {
+            const bookDetails = await getBookDetails(browser, cookies[i]);
+            await browser.close();
 
-    } catch (e) {
-        const screenshot = await e.page.screenshot();
-        const params = { Bucket: bucket, Key: key, Body: screenshot };
-        await s3.putObject(params).promise();
-        console.log('something happened See screenshot in s3 bucket', e.error);
-        await browser.close();
-        return e.error;
+            return bookDetails;
+
+        } catch (e) {
+            const screenshot = await e.page.screenshot();
+            const params = { Bucket: bucket, Key: key, Body: screenshot };
+            await s3.putObject(params).promise();
+            console.log('something happened See screenshot in s3 bucket', e.error);
+            await browser.close();
+            return e.error;
+        }
     }
 }
 
-async function getBookDetails(browser: Browser) {
+async function getBookDetails(browser: any, cookies: string) {
     const url = 'https://www.audible.com/';
-    const context = await browser.createIncognitoBrowserContext();
-    const page = await auth(context, process.env.jacobCookie);
+
+    const page = await auth(browser, cookies);
     try {
-        // await page.goto(url);
-        // await page.click('.ui-it-sign-in-link');
-        // await page.waitForSelector('#ap_email', { timeout: 5000 });
-        // await page.type('#ap_email', process.env.email);
-        // await page.type('#ap_password', process.env.password);
-
-        // await page.click('#signInSubmit');
-
-        // console.log("Sign in link");
-        // console.log("before titles page");
-        // await page.waitForSelector('.bc-pub-block.ui-it-header-logo', { timeout: 5000 });
-        await page.goto('https://audible.com/library/titles', {
-            waitUntil: 'networkidle0',
-            timeout: 30000
-        });
-        console.log("after titles page");
-        const pageNumberLast: number = +await page.$eval('.linkListWrapper li:nth-last-of-type(2) .bc-link', element => element.getAttribute('data-value'));
+        let pageNumberLast: number;
+        try {
+            pageNumberLast = +await page.$eval('.linkListWrapper li:nth-last-of-type(2) .bc-link', element => element.getAttribute('data-value'));
+        } catch (e) {
+            pageNumberLast = 2;
+        }
+        // const pageNumberLast: number = +await page.$eval('.linkListWrapper li:nth-last-of-type(2) .bc-link', element => element.getAttribute('data-value'));
         console.log('page number', pageNumberLast);
-        // const pageNumber: number = +pageNumberString;
+        let owner = await page.$eval('.bc-text.navigation-do-underline-on-hover.ui-it-barker-text', element => element.textContent);
+        owner = owner.split(',')[1].replace('!', '').trim();
+
         let pageNumber: number = 1;
-        // let libraryHasBooks = true;
+
+        const existingTitles = await scanTitles();
+
         const bookData: any[] = [];
         while (pageNumber <= pageNumberLast) {
             await page.goto(`${url}/library/titles?ref=a_library_t_c6_pageSize_0&pf_rd_p=754864b9-4c5c-4301-b92a-69b12a5623c4&pf_rd_r=2793CX9HNJGABE75EYEX&piltersCurrentValue=All&sortBy=PURCHASE_DATE.dsc&pageSize=20&page=${pageNumber}`);
@@ -82,7 +71,6 @@ async function getBookDetails(browser: Browser) {
             for (let bookHandle of booksHandle) {
                 const title = await bookHandle.$eval('.bc-text.bc-size-headline3', element => element.textContent);
                 const author = await bookHandle.$eval('.bc-text.bc-size-callout', element => element.textContent);
-                const image = await bookHandle.$eval('img', element => element.getAttribute('src'));
                 let url: string;
                 try {
                     url = await bookHandle.$eval('.bc-list-item:nth-of-type(1) .bc-link.bc-color-base', element => element.getAttribute('href'));
@@ -90,33 +78,36 @@ async function getBookDetails(browser: Browser) {
                 catch (e) {
                     console.log('Cannot find url. Moving on.');
                 }
-                bookData.push({
-                    title: title.trim(),
-                    author: author.trim(),
-                    image: image,
-                    url: url,
-                });
+                if (!existingTitles.find(book => book.title === title) && url) {
+                    bookData.push({
+                        title: title.trim(),
+                        author: author.trim(),
+                        url: url,
+                        owner: owner
+                    });
+                }
+
+
             }
             pageNumber++;
 
         }
 
         console.log('bookData', bookData);
-        // const inputParams: aws.DynamoDB.DocumentClient.PutItemInput = {
-        //     Item: bookData,
-        //     TableName: 'audible-libraries'
-        // };
-        const params = {
-            TableName: 'audible-libraries',
-            Item: {
-                id: uuidv4(),
-                title: bookData[0].title,
-                author: bookData[0].author,
+        for (let i = 0; i < bookData.length; i++) {
 
-            }
-        };
-        await documentClient.put(params).promise();
-        await context.close();
+            const params = {
+                TableName: 'audible-libraries',
+                Item: {
+                    id: uuidv4(),
+                    title: bookData[i].title,
+                    author: bookData[i].author,
+                    url: bookData[i].url,
+                    owner: bookData[i].owner
+                }
+            };
+            await documentClient.put(params).promise();
+        }
 
         return bookData;
     } catch (e) {
@@ -124,16 +115,33 @@ async function getBookDetails(browser: Browser) {
         const params = { Bucket: bucket, Key: key, Body: screenshot };
         await s3.putObject(params).promise();
         console.log('something happened See screenshot in s3 bucket', e);
-        await context.close();
+
     }
-
-
 }
 
-async function auth(context: puppeteer.BrowserContext, cookie: string) {
+async function scanTitles(existingTitles = [], lastEvaluatedKey?: aws.DynamoDB.DocumentClient.Key) {
 
-    const page = await context.newPage();
+    const scanParams: aws.DynamoDB.DocumentClient.ScanInput = {
+        TableName: 'audible-libraries',
+        ProjectionExpression: 'title'
+    };
+    if (lastEvaluatedKey) {
+        scanParams.ExclusiveStartKey = lastEvaluatedKey;
+    }
+    const titleResponse = await documentClient.scan(scanParams).promise();
+    existingTitles.push(...titleResponse.Items);
 
+    if (titleResponse.LastEvaluatedKey) {
+        return await scanTitles(existingTitles, titleResponse.LastEvaluatedKey);
+    }
+
+    return existingTitles;
+}
+
+
+async function auth(browser: any, cookie: string) {
+
+    const page = await browser.newPage();
     const cookies: puppeteer.Cookie[] = [
         {
             name: "x-main",
@@ -145,7 +153,10 @@ async function auth(context: puppeteer.BrowserContext, cookie: string) {
     ];
 
     await page.goto('https://audible.com');
+
     // Set cookie and then go to library
     await page.setCookie(...cookies);
+    const libraryUrl = 'https://audible.com/library/titles';
+    await page.goto(libraryUrl);
     return page;
 }
